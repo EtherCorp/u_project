c243ae8396034fc95710b9d80a9b0df6271c97cf
Mario Álvarez Molina <mario.alvarez.m@usach.cl>
2017-10-11 02:05:57 -0300

    Added more REST  methods to MovementType, Patient, Professional and Speciality

--- unicorn-api/app/api/api.rb
+++ unicorn-api/app/api/api.rb
@@ -87,6 +87,17 @@ module API
         present MedicalCenter.all, with: Entities::MedicalCenter
       end
 
+      desc 'Create medical center'
+      params do
+        requires :name, type: String, desc: 'Medical center name'
+        requires :address, type: String, desc: 'Address'
+      end
+      post do
+        med_center = MedicalCenter.new params
+        med_center.save!
+        med_center
+      end
+
       route_param :id do
         desc 'Get medical center by ID'
         get do
@@ -145,6 +156,13 @@ module API
         get do
           present Patient.find_by(id: params[:id]), with: Entities::Patient
         end
+
+        resource :consults do
+          desc 'Get all consults from a patient'
+          get do
+            present Patient.find_by(id: params[:id]).consults, with: Entities::Consult
+          end
+        end
       end
     end
 
@@ -182,6 +200,13 @@ module API
         get do
           present Professional.find_by(id: params[:id]),
                   with: Entities::Professional
+        end
+
+        resource :consults do
+          desc 'Get all consults from a professional'
+          get do
+            present Professional.find_by(id: params[:id]).consults, with: Entities::Consult
+          end
         end
 
         resource :medical_centers do
